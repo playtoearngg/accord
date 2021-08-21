ae445c17414ba4c4f580809117b0ca585e97c3e3
Deckard <deckard@radgamingguild.com>
2021-08-21 19:51:20 +0800

    client: Add thread emoji to names of created threads

--- client.py
+++ client.py
@@ -5,3 +5,10 @@ class AccordClient(Client):
     async def on_ready(self):
         # Hello, world
         print('Logged on as {0}!'.format(self.user))
+
+    async def on_thread_join(self, thread):
+        # Thread created, check if the name has the thread emoji
+        thread_emoji = "🧵"
+
+        if thread_emoji not in thread.name:
+            await thread.edit(name='{0}{1}'.format(thread_emoji, thread.name))
