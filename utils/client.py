5f8c6f12f2862c492bdad9c99b0d9714c39bf8e3
Deckard <deckard@radgamingguild.com>
2021-08-24 15:33:38 +0800

    chore(utils): Comments formatting

--- utils/client.py
+++ utils/client.py
@@ -3,11 +3,14 @@ from discord import Client
 
 
 class AccordClient(Client):
+    # Hello, world
     async def on_ready(self):
-        # Hello, world
         print('Logged on as {0}!'.format(self.user))
 
+    # On thread create
     async def on_thread_join(self, thread):
-        # On thread create
-        await thread.edit(locked=True)          # Only mods can archive/unarchive
+        # Add thread emoji to title
         await add_thread_emoji(thread)
+
+        # Only mods can archive/unarchive
+        await thread.edit(locked=True)
